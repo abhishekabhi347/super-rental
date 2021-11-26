8372ee715f22a29722ae30516c04dff6a8e70626
Abhishek Rampi <abhishekrampi@Abhisheks-MacBook-Air.local>
2021-11-26 18:22:15 +0530

    added map component

--- config/environment.js
+++ config/environment.js
@@ -46,6 +46,8 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1IjoiYWJoaXNoZWtyIiwiYSI6ImNrd2didzQ3OTBuM3cycW5zNWgzdzM1MWoifQ.cWg_9JBAVs0LIAN61fvS3g';
 
   return ENV;
 };
